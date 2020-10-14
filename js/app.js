dfdeb1c9be23d8ac614160764bbbe375c01284a3
jacob kleiman <jacobakleiman@gmail.com>
2020-10-14 12:26:17 -0700

    adds change listener

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 console.log('🌴');
 const el = document.querySelector('.multi');
+const opt = [];
+const addOpt = (value) => {
+  opt.push(value);
+};
+el.addEventListener('change', (e) => addOpt(e.target.value));
