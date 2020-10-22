e39a1ef45c62a1e42317f2ac24fe5841222cde9e
jacob kleiman <jacobakleiman@gmail.com>
2020-10-22 13:40:51 -0700

    adds selected buffer, event listeners

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 console.log('🌴');
 const el = document.querySelector('.multi');
+// stores values of dropdown
+const selected = [];
 // array of options
 const { options: opt } = el;
 
@@ -16,14 +18,31 @@ const dropdown = document.createElement('div');
 dropdown.classList.add('dropdown')
 // create ul
 const list = document.createElement('ul');
+list.classList.add('hidden');
 // iterate over options, skip first (placeholder)
 for (let i = 1; i < opt.length; i++) {
     // console.log(opt[i].innerText);
     const li = document.createElement('li');
     li.classList.add('item');
     li.innerText=opt[i].innerText
+    li.addEventListener('click', e => {
+        selected.push(e.target.innerText);
+        e.target.parentElement.removeChild(e.target);
+        
+    })
     list.appendChild(li);
 }
+// add event listener to window
+document.addEventListener('click', e => {
+    list.classList.add('hidden');
+})
+// add event listener to container to show / hide dropdown
+container.addEventListener('click', e => {
+    e.stopPropagation();
+    list.classList.toggle('hidden');
+})
+
+
 // append dropdown to container
 container.appendChild(dropdown)
 // append ul to dropdown
