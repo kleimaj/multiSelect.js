acc42a75e96e628fd41a0bcea5f41429b58a3f25
jacob kleiman <jacobakleiman@gmail.com>
2020-10-22 13:19:05 -0700

    adds unordered list and list items

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const el = document.querySelector('.multi');
 const { options: opt } = el;
 
 const container = document.createElement('div');
+container.classList.add('container')
 // set innerText to default value
 container.innerText = opt[0].innerText;
 console.log(container);
@@ -12,7 +13,20 @@ console.log(container);
 document.querySelector('#root').appendChild(container);
 // create container
 const dropdown = document.createElement('div');
+dropdown.classList.add('dropdown')
+// create ul
+const list = document.createElement('ul');
+// iterate over options, skip first (placeholder)
+for (let i = 1; i < opt.length; i++) {
+    // console.log(opt[i].innerText);
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.innerText=opt[i].innerText
+    list.appendChild(li);
+}
 // append dropdown to container
 container.appendChild(dropdown)
+// append ul to dropdown
+dropdown.appendChild(list);
 // remove select field
 el.parentElement.removeChild(el);
