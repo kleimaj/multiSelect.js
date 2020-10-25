bc9a423f7d0ab323e423d5d38729b8ddb1bd31ce
jacob kleiman <jacobakleiman@gmail.com>
2020-10-24 17:34:28 -0700

    refactors code for multiple multi-selects

--- js/app.js
+++ js/app.js
@@ -7,8 +7,11 @@ const updateContainer = (display,inner, selected, opt) => {
         inner.innerText = opt[0].innerText;
     }
 }
+const map = {};
 const multiSelect = () => {
-    document.querySelectorAll('.multi').forEach((el) => {
+    document.querySelectorAll('.multi').forEach((el, idx) => {
+        // stores values of dropdown
+        map[idx] = [];
         // array of options
         const { options: opt } = el;
         // create container
@@ -31,92 +34,148 @@ const multiSelect = () => {
         container.appendChild(display);
         display.appendChild(inner);
 
-    })
-}
+        // replace new select element with new container
+        el.parentNode.replaceChild(container, el);
 
-const el = document.querySelector('.multi');
-// stores values of dropdown
-const selected = [];
-// array of options
-const { options: opt } = el;
-
-// create container
-const container = document.createElement('div');
-container.classList.add('container')
-
-// create display
-const display = document.createElement('button');
-display.classList.add('display');
-
-// create inner (displays the values)
-const inner = document.createElement('div');
-inner.classList.add('inner');
-
-// set innerText to default value
-// display.innerText = opt[0].innerText
-inner.innerText = opt[0].innerText
-display.value = ""
-// append display to container
-container.appendChild(display);
-display.appendChild(inner);
-// container.innerText = opt[0].innerText;
-// console.log(container);
-// insert container
-// el.insertAdjacentHTML('beforebegin', container);
-// document.querySelector('#root').appendChild(container);
-el.parentNode.replaceChild(container, el);
-// create container
-const dropdown = document.createElement('div');
-dropdown.classList.add('dropdown')
-// create ul
-const list = document.createElement('ul');
-dropdown.classList.add('hidden');
-// iterate over options, skip first (placeholder)
-for (let i = 1; i < opt.length; i++) {
-    // console.log(opt[i].innerText);
-    const li = document.createElement('li');
-    li.classList.add('item');
-    // ACCESSIBILITY
-    // const a = document.createElement('a');
-    // a.classList.add('item-anchor');
-    // a.setAttribute('role','option');
-    // a.setAttribute('tabindex','0');
-    // a.innerText=opt[i].innerText
-    li.innerText=opt[i].innerText;
-    li.addEventListener('click', e => {
-        e.stopPropagation();
-        if (selected.includes(e.target.innerText)) {
-            selected.splice(selected.indexOf(e.target.innerText), 1);
-            e.target.classList.remove('selected');
-        }else {
-            selected.push(e.target.innerText);
-            e.target.classList.add('selected');
+        // create dropdown
+        const dropdown = document.createElement('div');
+        dropdown.classList.add('dropdown')
+        // create ul
+        const list = document.createElement('ul');
+        dropdown.classList.add('hidden');
+        // iterate over options, skip first (placeholder)
+        for (let i = 1; i < opt.length; i++) {
+            const li = document.createElement('li');
+            li.classList.add('item');
+            // ACCESSIBILITY
+            // const a = document.createElement('a');
+            // a.classList.add('item-anchor');
+            // a.setAttribute('role','option');
+            // a.setAttribute('tabindex','0');
+            // a.innerText=opt[i].innerText
+            li.innerText=opt[i].innerText;
+            li.addEventListener('click', e => {
+                e.stopPropagation();
+                if (map[idx].includes(e.target.innerText)) {
+                    map[idx].splice(map[idx].indexOf(e.target.innerText), 1);
+                    e.target.classList.remove('selected');
+                }else {
+                    map[idx].push(e.target.innerText);
+                    e.target.classList.add('selected');
+                }
+                // e.target.parentElement.removeChild(e.target);
+                updateContainer(display, inner, map[idx], opt);
+            })
+            // li.appendChild(a);
+            list.appendChild(li);
         }
-        // e.target.parentElement.removeChild(e.target);
-        updateContainer(display, inner, selected, opt);
+        // add event listener to container to show / hide dropdown
+        container.addEventListener('click', e => {
+            e.stopPropagation();
+            display.classList.toggle('dropdown-toggle');
+            dropdown.classList.toggle('hidden');
+        })
+        // append dropdown to container
+        container.appendChild(dropdown)
+        // append ul to dropdown
+        dropdown.appendChild(list);
     })
-    // li.appendChild(a);
-    list.appendChild(li);
+    // add event listener to window
+    document.addEventListener('click', e => {
+        document.querySelectorAll('.display').forEach((display) => {
+            display.classList.remove('dropdown-toggle');
+        })
+
+        document.querySelectorAll('.dropdown').forEach((dropdown) => {
+            dropdown.classList.add('hidden');
+        })
+    })
 }
-// add event listener to window
-document.addEventListener('click', e => {
-    display.classList.remove('dropdown-toggle');
-    dropdown.classList.add('hidden');
-})
-// add event listener to container to show / hide dropdown
-container.addEventListener('click', e => {
-    e.stopPropagation();
-    display.classList.toggle('dropdown-toggle');
-    dropdown.classList.toggle('hidden');
-})
+multiSelect()
+
+// const el = document.querySelector('.multi');
+// // stores values of dropdown
+// const selected = [];
+// // array of options
+// const { options: opt } = el;
+
+// // create container
+// const container = document.createElement('div');
+// container.classList.add('container')
+
+// // create display
+// const display = document.createElement('button');
+// display.classList.add('display');
+
+// // create inner (displays the values)
+// const inner = document.createElement('div');
+// inner.classList.add('inner');
+
+// // set innerText to default value
+// // display.innerText = opt[0].innerText
+// inner.innerText = opt[0].innerText
+// display.value = ""
+// // append display to container
+// container.appendChild(display);
+// display.appendChild(inner);
+// // container.innerText = opt[0].innerText;
+// // console.log(container);
+// // insert container
+// // el.insertAdjacentHTML('beforebegin', container);
+// // document.querySelector('#root').appendChild(container);
+// el.parentNode.replaceChild(container, el);
+// // create dropdown
+// const dropdown = document.createElement('div');
+// dropdown.classList.add('dropdown')
+// // create ul
+// const list = document.createElement('ul');
+// dropdown.classList.add('hidden');
+// // iterate over options, skip first (placeholder)
+// for (let i = 1; i < opt.length; i++) {
+//     // console.log(opt[i].innerText);
+//     const li = document.createElement('li');
+//     li.classList.add('item');
+//     // ACCESSIBILITY
+//     // const a = document.createElement('a');
+//     // a.classList.add('item-anchor');
+//     // a.setAttribute('role','option');
+//     // a.setAttribute('tabindex','0');
+//     // a.innerText=opt[i].innerText
+//     li.innerText=opt[i].innerText;
+//     li.addEventListener('click', e => {
+//         e.stopPropagation();
+//         if (selected.includes(e.target.innerText)) {
+//             selected.splice(selected.indexOf(e.target.innerText), 1);
+//             e.target.classList.remove('selected');
+//         }else {
+//             selected.push(e.target.innerText);
+//             e.target.classList.add('selected');
+//         }
+//         // e.target.parentElement.removeChild(e.target);
+//         updateContainer(display, inner, selected, opt);
+//     })
+//     // li.appendChild(a);
+//     list.appendChild(li);
+// }
+// // add event listener to window
+// document.addEventListener('click', e => {
+//     display.classList.remove('dropdown-toggle');
+    // dropdown.classList.add('hidden');
+// })
+// // add event listener to container to show / hide dropdown
+// container.addEventListener('click', e => {
+//     e.stopPropagation();
+//     display.classList.toggle('dropdown-toggle');
+//     dropdown.classList.toggle('hidden');
+// })
 
 
-// append dropdown to container
-container.appendChild(dropdown)
-// append ul to dropdown
-dropdown.appendChild(list);
+// // append dropdown to container
+// container.appendChild(dropdown)
+// // append ul to dropdown
+// dropdown.appendChild(list);
 // remove select field
-el.parentElement.removeChild(el);
+// el.parentElement.removeChild(el);
 
 // const button = document.createElement('button');
 // button.style.backgroundImage="url(./svg/angle-down-light.svg)";
